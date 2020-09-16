8ab5344538d74d8b1e0061d800a54ed92d60b779
Svetlana Antipova <die-chimaere@yandex.ru>
2020-09-16 12:46:53 +0300

    change router basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-mesto-auth">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode >,
